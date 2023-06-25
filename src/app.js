3ec2875915f02693aaedfe783ce5185f7e20cde0
Pablo Morelli <pablojm21@hotmail.com>
2023-06-24 21:50:09 -0300

    Se instalo y verifico el funcionamiento del framework Express.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo!");
+const express = require('express');
+const PORT = 8090
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("¡Bienvenido a mi aplicación!");
+    });
+
+app.get('/hello', (req, res) => {
+  res.send(`Hello, ${req.query.person}!`);
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
